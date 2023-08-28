ed1bd4dbe9cfa3dfad5e65810393a57359d24ff1
william-spongberg <william.spongberg@outlook.com>
2023-08-28 18:24:02 +1000

    fixed how notes are displayed

--- src/Note.java
+++ src/Note.java
@@ -1,4 +1,5 @@
 import bagel.*;
+import bagel.util.Colour;
 
 public class Note {
     // images
@@ -13,26 +14,31 @@ public class Note {
     private final Image IMAGE_NOTEHOLD_DOWN = new Image("res/holdNoteDown.png");
 
     // directions
-    private final int LEFT = 0;
-    private final int RIGHT = 1;
-    private final int UP = 2;
-    private final int DOWN = 3;
+    private static final int LEFT = 0;
+    private static final int RIGHT = 1;
+    private static final int UP = 2;
+    private static final int DOWN = 3;
 
     // position
-    private int y = 0;
-    private final int start_y = 100;
+    private static final int START_Y = 100;
 
     // attributes with default values
     private Image image = IMAGE_NOTEHOLD_LEFT;
     private int delay = 0;
     private int dir = LEFT;
+    private int y = 0;
     private boolean is_held = false;
+    private boolean visual = false;
+    private boolean active = true;
 
-    
+    // font [testing]
+    private final static String FILE_FONT = "res/FSO8BITR.TTF";
+    private final Font FONT_SMALL = new Font(FILE_FONT, 12);
+
     public Note(String dir, String type, int delay) {
         if (type == "Hold") {
             this.is_held = true;
-            
+
             switch (dir) {
                 case "Left":
                     this.image = IMAGE_NOTEHOLD_LEFT;
@@ -79,8 +85,26 @@ public class Note {
     }
 
     public void update(int frame, int lane_x[]) {
-        // draw note
-        this.image.drawFromTopLeft(lane_x[this.dir], start_y + this.y);
+        if (active) {
+            // calculate y position
+            this.y = (START_Y/4 + frame - this.delay)*4;
+            // if note is on screen
+            if (this.y > START_Y) {
+                // then is visual
+                if (!this.visual)
+                    this.visual = true;
+                // draw note
+                this.image.draw(lane_x[this.dir], this.y);
+
+                // draw note position [testing]
+                /*DrawOptions opt = new DrawOptions();
+                opt.setBlendColour(Colour.RED);
+                FONT_SMALL.drawString("" + this.y, lane_x[this.dir] - FONT_SMALL.getWidth("" + this.y) / 2, this.y,
+                        opt);
+                FONT_SMALL.drawString("" + (657 - this.y), lane_x[this.dir] - FONT_SMALL.getWidth("" + this.y) / 2,
+                        this.y + 10, opt);*/
+            }
+        }
     }
 
     public Image getImage() {
@@ -95,7 +119,23 @@ public class Note {
         return this.delay;
     }
 
-    public Boolean getIsHeld() {
+    public Integer getY() {
+        return this.y;
+    }
+
+    public Boolean isHeld() {
         return this.is_held;
+    }
+
+    public Boolean isVisual() {
+        return this.visual;
+    }
+
+    public Boolean isActive() {
+        return this.active;
+    }
+
+    public void endActive() {
+        this.active = false;
     }
 }
