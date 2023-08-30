73d811659ffc8e460f141d98f1746451374ee204
william-spongberg <william.spongberg@outlook.com>
2023-08-30 15:42:24 +1000

    removed scoring, added reset functionality

--- src/Level.java
+++ src/Level.java
@@ -1,9 +1,8 @@
-import bagel.*;
-import bagel.util.Colour;
-import bagel.util.Point;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import bagel.Image;
+import bagel.Input;
 
 public class Level {
     // images
@@ -12,15 +11,11 @@ public class Level {
     private final Image IMAGE_LANE_UP = new Image("res/laneUp.png");
     private final Image IMAGE_LANE_DOWN = new Image("res/laneDown.png");
 
-    // positions
-    private static final int LANE_Y = 384;
-    private static final int NOTE_STATIONARY_Y = 657;
+    // display text
+    DisplayText disp = new DisplayText();
 
-    // scoring distances
-    private static final int PERFECT = 15;
-    private static final int GOOD = 50;
-    private static final int BAD = 100;
-    private static final int MISS = 200;
+    // position
+    private static final int LANE_Y = 384;
 
     // csv file indices
     private static final int INDEX_LANE = 0;
@@ -40,12 +35,80 @@ public class Level {
     private static int num = -1;
     private int score = 0;
     private Note currentNote;
+    private boolean holding = false;
+    private int grade = 0;
 
     public Level(List<List<String>> file) {
+        reset(file);
+    }
+
+    public void update(int frame, Input input) {
+        Grade grade = new Grade();
+        if (currentNote.isActive()) {
+            // check score while current note is active
+            Boolean[] array = grade.checkScore(currentNote, input, this.holding);
+            // set new holding and active states
+            currentNote.setActive(array[0]);
+            this.holding = array[1];
+        }
+
+        // if current note inactive
+        if (!currentNote.isActive()) {
+            // get next note if more left
+            if (notes.indexOf(currentNote) < notes.size() - 1) {
+                currentNote = notes.get(notes.indexOf(currentNote) + 1);
+            } else {
+                // if no more notes, end level
+                this.active = false;
+            }
+        }
+
+        // update grade and score
+        this.grade = grade.getGrade();
+        this.score += this.grade;
+
+        // draw lanes
+        IMAGE_LANE_LEFT.draw(lane_x[LEFT], LANE_Y);
+        IMAGE_LANE_RIGHT.draw(lane_x[RIGHT], LANE_Y);
+        IMAGE_LANE_UP.draw(lane_x[UP], LANE_Y);
+        IMAGE_LANE_DOWN.draw(lane_x[DOWN], LANE_Y);
+
+        // draw scoring positions [testing]
+        /* 
+        Point a = new Point(0, NOTE_STATIONARY_Y - 100);
+        Point b = new Point(2000, NOTE_STATIONARY_Y - 100);
+        Drawing.drawLine(a, b, 200, Colour.WHITE);
+        a = new Point(0, NOTE_STATIONARY_Y - 50);
+        b = new Point(2000, NOTE_STATIONARY_Y - 50);
+        Drawing.drawLine(a, b, 100, Colour.RED);
+        a = new Point(0, NOTE_STATIONARY_Y - 25);
+        b = new Point(2000, NOTE_STATIONARY_Y - 25);
+        Drawing.drawLine(a, b, 50, Colour.BLUE);
+        a = new Point(0, NOTE_STATIONARY_Y - 15 / 2);
+        b = new Point(2000, NOTE_STATIONARY_Y - 15 / 2);
+        Drawing.drawLine(a, b, 15, Colour.GREEN);
+        /* */
+
+        // draw distance to note [testing]
+        /* 
+        a = new Point(lane_x[currentNote.getDir()], currentNote.getY());
+        b = new Point(lane_x[currentNote.getDir()], NOTE_STATIONARY_Y);
+        Drawing.drawLine(a, b, 5, Colour.BLUE);
+        /* */
+
+        // draw notes
+        for (Note note : this.notes) {
+            note.update(frame, lane_x);
+        }        
+    }
+
+    private void reset(List<List<String>> file) {
         // print out csv file [testing]
+        /* */
         for (List<String> record : file) {
             System.out.println(record);
         }
+        /* */
 
         // create objects from csv file
         int i = 0;
@@ -68,88 +131,6 @@ public class Level {
         num++;
     }
 
-    public void update(int frame, Input input) {
-        // draw lanes
-        IMAGE_LANE_LEFT.draw(lane_x[0], LANE_Y);
-        IMAGE_LANE_RIGHT.draw(lane_x[1], LANE_Y);
-        IMAGE_LANE_UP.draw(lane_x[2], LANE_Y);
-        IMAGE_LANE_DOWN.draw(lane_x[3], LANE_Y);
-
-        // draw scoring positions [testing]
-        /*Point a = new Point(0, NOTE_STATIONARY_Y - 100);
-        Point b = new Point(2000, NOTE_STATIONARY_Y - 100);
-        Drawing.drawLine(a, b, 200, Colour.WHITE);
-        a = new Point(0, NOTE_STATIONARY_Y - 50);
-        b = new Point(2000, NOTE_STATIONARY_Y - 50);
-        Drawing.drawLine(a, b, 100, Colour.RED);
-        a = new Point(0, NOTE_STATIONARY_Y - 25);
-        b = new Point(2000, NOTE_STATIONARY_Y - 25);
-        Drawing.drawLine(a, b, 50, Colour.BLUE);
-        a = new Point(0, NOTE_STATIONARY_Y - 15 / 2);
-        b = new Point(2000, NOTE_STATIONARY_Y - 15 / 2);
-        Drawing.drawLine(a, b, 15, Colour.GREEN);*/
-
-        // draw notes
-        for (Note note : this.notes) {
-            note.update(frame, lane_x);
-        }
-
-        // get input
-        Integer dir = checkInput(input);
-
-        // check score
-        if (!checkScore(currentNote, dir)) {
-            // if note is inactive, set next note to current note
-            if (notes.indexOf(currentNote) < notes.size() - 1)
-                currentNote = notes.get(notes.indexOf(currentNote) + 1);
-        }
-    }
-
-    public Boolean checkScore(Note note, int dir) {
-        // if input matches note and is in scoring range
-        if (dir != -1 && dir == note.getDir() && (NOTE_STATIONARY_Y - note.getY() <= MISS)) {
-            // if scored, delete
-            if (NOTE_STATIONARY_Y - note.getY() <= PERFECT) {
-                this.score += 10;
-                note.endActive();
-                System.out.println("PERFECT");
-            } else if (NOTE_STATIONARY_Y - note.getY() <= GOOD) {
-                this.score += 5;
-                note.endActive();
-                System.out.println("GOOD");
-            } else if (NOTE_STATIONARY_Y - note.getY() <= BAD) {
-                this.score -= 1;
-                note.endActive();
-                System.out.println("BAD");
-                // if missed, don't delete
-            } else if ((NOTE_STATIONARY_Y - note.getY() <= MISS)) {
-                this.score -= 5;
-                System.out.println("MISS");
-            }
-            // else if past scoring range, delete
-            else if (NOTE_STATIONARY_Y - note.getY() < 0) {
-                note.endActive();
-                System.out.println("deleted node");
-            }
-        }
-        return note.isActive();
-    }
-
-    public Integer checkInput(Input input) {
-        // check for input
-        if (input.wasReleased(Keys.LEFT)) {
-            return LEFT;
-        } else if (input.wasReleased(Keys.RIGHT)) {
-            return RIGHT;
-        } else if (input.wasReleased(Keys.UP)) {
-            return UP;
-        } else if (input.wasReleased(Keys.DOWN)) {
-            return DOWN;
-        } else {
-            return -1;
-        }
-    }
-
     public Boolean isActive() {
         return this.active;
     }
@@ -158,11 +139,15 @@ public class Level {
         this.active = false;
     }
 
-    public Integer getNum() {
+    public static Integer getNum() {
         return num;
     }
 
     public Integer getScore() {
         return this.score;
+    }
+
+    public Integer getGrade() {
+        return this.grade;
     }
 }
