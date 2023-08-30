8e77a037114fd5f6908558d5aa3b1f77ab4135f5
william-spongberg <william.spongberg@outlook.com>
2023-08-30 15:44:07 +1000

    removed unnecessary strings, added grading logic

--- src/ShadowDance.java
+++ src/ShadowDance.java
@@ -12,41 +12,40 @@ import java.util.List;
  * @author William Spongberg
  */
 public class ShadowDance extends AbstractGame {
-    // window dimensions, title and background
+    // window dimensions, refresh rate, background and title
     private final static int WINDOW_WIDTH = 1024;
     private final static int WINDOW_HEIGHT = 768;
-    private final static String GAME_TITLE = "SHADOW DANCE";
+    private final static String REFRESH_RATE = "-60"; // set to empty for 120hz
     private final Image IMAGE_BACKGROUND = new Image("res/background.png");
-
-    // font
-    private final static String FILE_FONT = "res/FSO8BITR.TTF";
-    private final Font FONT = new Font(FILE_FONT, 64);
-    private final Font FONT_SMALL = new Font(FILE_FONT, 24);
-
-    // title screen strings
-    private static final String TITLE = "SHADOW DANCE";
-    private static final String START_1 = "PRESS SPACE TO START";
-    private static final String START_2 = "USE ARROW KEYS TO PLAY";
-    private static final String PAUSED = "PAUSED";
-
-    // file level names
-    private static final String FILE_LEVEL1 = "res/test1-60.csv";
+    private final static String GAME_TITLE = "SHADOW DANCE";
 
     // game logic booleans
     private boolean paused = false;
     private boolean started = true;
     private boolean ended = false;
 
-    // frame counter
+    // frame counter, grading frame counter and score
     private int frame = 0;
+    private static final int GRADE_FRAMES = 30;
+    private int frames_grading = 0;
+    private int current_grade = 0;
+    private int score = 0;
 
-    // any number of levels
+    // display text
+    DisplayText disp = new DisplayText();
+
+    // array list of levels
     private List<Level> levels = new ArrayList<Level>();
+
+    // file level names
+    private static final String FILE_LEVEL1 = "res/levels/level1"+REFRESH_RATE+".csv";
 
     public ShadowDance() {
         super(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE);
 
+        // add levels
         this.levels.add(readCSV(FILE_LEVEL1));
+        // etc
     }
 
     /**
@@ -62,6 +61,7 @@ public class ShadowDance extends AbstractGame {
                 String[] values = line.split(",");
                 records.add(Arrays.asList(values));
             }
+            // catch and print errors
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -81,20 +81,21 @@ public class ShadowDance extends AbstractGame {
      * Allows the game to exit when the escape key is pressed.
      * Allows the game to pause when the tab key is pressed.
      * 
+     * Contains game state logic.
+     * 
      * Refreshed at user monitor refresh rate (typically 60Hz, but
      * will be marked on a 120hz monitor).
-     * 
-     * @param input The current mouse/keyboard state.
      */
     @Override
     protected void update(Input input) {
-        // even if paused:
         // draw background
-        IMAGE_BACKGROUND.draw(Window.getWidth() / 2.0, Window.getHeight() / 2.0);
+        disp.drawBackground(IMAGE_BACKGROUND);
+
         // allow user to exit
         if (input.wasPressed(Keys.ESCAPE)) {
             Window.close();
         }
+
         // pause logic
         if (input.wasPressed(Keys.TAB)) {
             if (!paused)
@@ -103,17 +104,10 @@ public class ShadowDance extends AbstractGame {
                 paused = false;
         }
 
-        // game logic
+        // game state logic
         if (paused) {
-            // pause screen - object/class/method?
-            FONT.drawString(PAUSED, WINDOW_WIDTH / 2 - FONT.getWidth(PAUSED) / 2, WINDOW_HEIGHT / 2);
-        } else { // if not paused
-            // increment frame counter
-            frame++;
-
-            // drawing frame counter [testing]
-            FONT_SMALL.drawString("Frame " + frame, WINDOW_WIDTH - FONT.getWidth("Frame " + frame)/3 - 50, 50);
-
+            disp.drawPauseScreen();
+        } else {
             // if space pressed start game
             if (input.wasPressed(Keys.SPACE)) {
                 started = false;
@@ -121,25 +115,66 @@ public class ShadowDance extends AbstractGame {
 
             // if game started
             if (started) {
-                // start screen - object/class/method?
-                FONT.drawString(TITLE, WINDOW_WIDTH / 2 - FONT.getWidth(TITLE) / 2, WINDOW_HEIGHT / 2 - 100);
-                FONT_SMALL.drawString(START_1, WINDOW_WIDTH / 2 - FONT_SMALL.getWidth(START_1) / 2, WINDOW_HEIGHT / 2);
-                FONT_SMALL.drawString(START_2, WINDOW_WIDTH / 2 - FONT_SMALL.getWidth(START_2) / 2,
-                        WINDOW_HEIGHT / 2 + 50);
+                disp.drawStartScreen();
             } else if (ended) { // if game ended
-                // end screen - object/class/method?
+                disp.drawEndScreen(score);
+                // if space pressed start game again
+                if (input.wasPressed(Keys.SPACE)) {
+                    // game restart [TO DO]
+                }
+            // if game in progress
+            } else {
+                // increment frame counter
+                frame++;
 
-            } else { // if game in progress
+                // draw frame counter [testing]
+                /* */
+                disp.drawFrame(frame);
+                /* */
+
                 // update levels
                 for (Level level : this.levels) {
-                    if (level.isActive())
-                        // draw frame counter [testing]
-                        FONT_SMALL.drawString("" + level.checkInput(input), 50, 100);
-                        level.update(frame, input);
+                    if (level.isActive()) {
                         // draw score
-                        FONT_SMALL.drawString("Score " + level.getScore(), 50, 50);
+                        disp.drawScore(level.getScore());
+
+                        // update level
+                        level.update(frame, input);
+                        
+
+                        // if grade to display
+                        if (level.getGrade() != 0) {
+                            frames_grading = GRADE_FRAMES;
+                            current_grade = level.getGrade();
+                        }
+
+                        // if frames left to display grade
+                        if (frames_grading > 0) {
+                            disp.drawGrade(current_grade);
+                            frames_grading--;
+                        }
+                        else {
+                            // reset grade
+                            current_grade = 0;
+                        }
+                        // add score to total score
+                        this.score += level.getScore();
+
+                        // if level ended, go to end screen
+                        if (!level.isActive()) {
+                            ended = true;
+                            score = level.getScore();
+                        }
+                    }
                 }
             }
         }
+    }
+    public static final int getWidth() {
+        return WINDOW_WIDTH;
+    }
+
+    public static final int getHeight() {
+        return WINDOW_HEIGHT;
     }
 }
