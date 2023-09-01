9ec809c331ddca710225dfee53edf41cd5dbe050
william-spongberg <william.spongberg@outlook.com>
2023-09-01 13:26:43 +1000

    removed testing, notes now score at end of screen

--- src/Level.java
+++ src/Level.java
@@ -1,11 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
-import bagel.Drawing;
 import bagel.Image;
 import bagel.Input;
-import bagel.util.Colour;
-import bagel.util.Point;
 
 public class Level {
     // images
@@ -16,9 +13,6 @@ public class Level {
 
     // position
     private static final int LANE_Y = 384;
-
-    // grade y position [testing]
-    private static final int NOTE_STATIONARY_Y = 657;
 
     // csv file indices
     private static final int INDEX_LANE = 0;
@@ -36,23 +30,18 @@ public class Level {
     private int[] lane_x = new int[4];
     private int score = 0;
     private int grade = 0;
-    private boolean holding = false; 
+    private boolean holding = false;
     private boolean active = false;
     private Boolean win = false;
     private Note currentNote;
 
+    // construct level
     public Level(List<List<String>> file) {
         reset(file);
     }
 
+    // reset level by csv file data
     public void reset(List<List<String>> file) {
-        // print out csv file [testing]
-        /* */
-        for (List<String> record : file) {
-            System.out.println(record);
-        }
-        /* */
-
         // create objects from csv file
         int i = 0;
         for (List<String> record : file) {
@@ -65,17 +54,17 @@ public class Level {
                         Integer.parseInt(record.get(INDEX_DELAY))));
             }
         }
-
         // get first note
-        this.currentNote = notes.get(0);
+        this.currentNote = this.notes.get(0);
     }
 
+    // reset level by level object data
     public void reset(Level level) {
         this.notes = level.getNotes();
-        for (Note note: notes) {
+        for (Note note : this.notes) {
             note.reset(note);
         }
-        this.currentNote = notes.get(0);
+        this.currentNote = this.notes.get(0);
 
         this.score = 0;
         this.grade = 0;
@@ -84,11 +73,15 @@ public class Level {
         this.active = false;
     }
 
+    // update level
     public void update(int frame, Input input) {
         Grade grade = new Grade();
         if (this.currentNote.isActive()) {
             // check score while current note is active
             Boolean[] array = grade.checkScore(this.currentNote, input, this.holding);
+            // update grade and score
+            this.grade = grade.getGrade();
+            this.score += this.grade;
             // set new holding and active states
             this.currentNote.setActive(array[0]);
             this.holding = array[1];
@@ -97,17 +90,24 @@ public class Level {
         // if current note now inactive
         if (!this.currentNote.isActive()) {
             // get next note if more left
-            if (notes.indexOf(this.currentNote) < notes.size() - 1) {
-                this.currentNote = notes.get(notes.indexOf(this.currentNote) + 1);
+            if (this.notes.indexOf(this.currentNote) < this.notes.size() - 1) {
+                this.currentNote = this.notes.get(this.notes.indexOf(this.currentNote) + 1);
+                this.holding = false;
+                // if last note isn't visual end level
             } else {
-                // if no more notes, end level
-                this.active = false;
+                if (!currentNote.isVisual())
+                    this.active = false;
             }
         }
 
-        // update grade and score
-        this.grade = grade.getGrade();
-        this.score += this.grade;
+        // check for notes below screen
+        for (Note note : this.notes) {
+            if (note.isBelowScreen()) {
+                this.grade = Grade.getMissGrade();
+                this.score += this.grade;
+                note.setBelowScreen(false);
+            }
+        }
 
         // draw lanes
         IMAGE_LANE_LEFT.draw(lane_x[LEFT], LANE_Y);
@@ -115,34 +115,15 @@ public class Level {
         IMAGE_LANE_UP.draw(lane_x[UP], LANE_Y);
         IMAGE_LANE_DOWN.draw(lane_x[DOWN], LANE_Y);
 
-        // draw scoring positions [testing]
-        /* 
-        Point a = new Point(0, NOTE_STATIONARY_Y - 100);
-        Point b = new Point(2000, NOTE_STATIONARY_Y - 100);
-        Drawing.drawLine(a, b, 200, Colour.WHITE);
-        a = new Point(0, NOTE_STATIONARY_Y - 50);
-        b = new Point(2000, NOTE_STATIONARY_Y - 50);
-        Drawing.drawLine(a, b, 100, Colour.RED);
-        a = new Point(0, NOTE_STATIONARY_Y - 25);
-        b = new Point(2000, NOTE_STATIONARY_Y - 25);
-        Drawing.drawLine(a, b, 50, Colour.BLUE);
-        a = new Point(0, NOTE_STATIONARY_Y - 15 / 2);
-        b = new Point(2000, NOTE_STATIONARY_Y - 15 / 2);
-        Drawing.drawLine(a, b, 15, Colour.GREEN);
-        /* */
-
         // draw notes
         for (Note note : this.notes) {
             note.update(frame, lane_x);
-        }        
+        }
     }
 
+    /* getters */
     public Boolean isActive() {
         return this.active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
     }
 
     public List<Note> getNotes() {
@@ -159,6 +140,15 @@ public class Level {
 
     public Boolean hasWin() {
         return this.win;
+    }
+
+    /* setters */
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 
     public void setWin(Boolean win) {
