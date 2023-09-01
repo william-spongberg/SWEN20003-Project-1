d93405662c1e4fb5544593c1b3f8d551d3f2b2a6
william-spongberg <william.spongberg@outlook.com>
2023-09-01 13:27:50 +1000

    removed testing, added comments, added boolean

--- src/Note.java
+++ src/Note.java
@@ -1,5 +1,4 @@
 import bagel.*;
-import bagel.util.Colour;
 
 public class Note {
     // images
@@ -39,10 +38,7 @@ public class Note {
     private boolean is_held = false;
     private boolean active = true;
     private boolean visual = false;
-
-    // font [testing]
-    private final static String FILE_FONT = "res/FSO8BITR.TTF";
-    private final Font FONT_SMALL = new Font(FILE_FONT, 20);
+    private boolean below_screen = false;
 
     public Note(String dir, String type, int delay) {
         reset(dir, type, delay);
@@ -55,41 +51,45 @@ public class Note {
             start_y = START_Y_HOLD;
         }
 
+        // set image direction, note direction to dir
         switch (dir) {
             case "Left":
                 if (this.is_held)
                     this.image = IMAGE_NOTEHOLD_LEFT;
-                else 
+                else
                     this.image = IMAGE_NOTE_LEFT;
                 this.dir = LEFT;
                 break;
             case "Right":
                 if (this.is_held)
                     this.image = IMAGE_NOTEHOLD_RIGHT;
-                else 
+                else
                     this.image = IMAGE_NOTE_RIGHT;
                 this.dir = RIGHT;
                 break;
             case "Up":
                 if (this.is_held)
                     this.image = IMAGE_NOTEHOLD_UP;
-                else 
+                else
                     this.image = IMAGE_NOTE_UP;
                 this.dir = UP;
                 break;
             case "Down":
                 if (this.is_held)
                     this.image = IMAGE_NOTEHOLD_DOWN;
-                else 
+                else
                     this.image = IMAGE_NOTE_DOWN;
                 this.dir = DOWN;
                 break;
             default:
                 System.out.println("Error: invalid note");
         }
+        // set note delay
         this.delay = delay;
     }
 
+    // reset to default values
+    // but keep original direction and delay
     public void reset(Note note) {
         this.image = note.getImage();
         this.delay = note.getDelay();
@@ -100,50 +100,36 @@ public class Note {
 
         this.active = true;
         this.visual = false;
+        this.below_screen = false;
     }
 
+    // update note position
     public void update(int frame, int lane_x[]) {
         // if active or visual
         if (this.active || this.visual) {
             // calculate y position
-            // if 60 hz
-            if (ShadowDance.hasRefresh60()) {
-                this.y = start_y + (frame - this.delay) * REFRESH_60_MULTIPLIER;
-            // else is 120 hz (according to marker)
-            } else {
-                this.y = start_y + (frame - this.delay) * REFRESH_120_MULTIPLIER;
-            }
+            this.y = start_y + (frame - this.delay)
+                    * ((ShadowDance.hasRefresh60()) ? REFRESH_60_MULTIPLIER : REFRESH_120_MULTIPLIER);
 
             // if note is on screen
-            if ((this.y > start_y) && (this.y < (ShadowDance.getHeight() + this.image.getWidth() / 2))) {
+            if ((this.y > start_y) && (this.y < (ShadowDance.getHeight() + this.image.getHeight() / 2))) {
                 // now visual
                 if (!this.visual)
                     this.visual = true;
-                
+
                 // draw note
                 this.image.draw(lane_x[this.dir], this.y);
-
-                // draw note position [testing]
-                /*
-                DrawOptions opt = new DrawOptions();
-                opt.setBlendColour(Colour.RED);  
-                FONT_SMALL.drawString("" + this.y, lane_x[this.dir] - FONT_SMALL.getWidth(""
-                        + this.y) / 2, this.y,
-                        opt);
-                opt.setBlendColour(Colour.GREEN);
-                FONT_SMALL.drawString("" + (657 - this.y), lane_x[this.dir] -
-                        FONT_SMALL.getWidth("" + this.y) / 2,
-                        this.y + 10, opt);
-                /* */
             }
-            // if not on screen
-            else if ((this.y > (ShadowDance.getHeight() + this.image.getWidth() / 2))) {
-                this.active = false;
+            // if note is below screen
+            else if (this.y > (ShadowDance.getHeight() + this.image.getHeight() / 2)) {
                 this.visual = false;
+                this.active = false;
+                this.below_screen = true;
             }
         }
     }
 
+    /* getters */
     public Image getImage() {
         return this.image;
     }
@@ -168,6 +154,19 @@ public class Note {
         return this.active;
     }
 
+    public Boolean isVisual() {
+        return this.visual;
+    }
+
+    public Boolean isBelowScreen() {
+        return this.below_screen;
+    }
+
+    public int getHeldMidpoint() {
+        return HOLD_MIDPOINT;
+    }
+
+    /* setters */
     public void setActive(Boolean active) {
         this.active = active;
     }
@@ -176,7 +175,7 @@ public class Note {
         this.visual = visual;
     }
 
-    public int getHeldMidpoint() {
-        return HOLD_MIDPOINT;
+    public void setBelowScreen(Boolean below_screen) {
+        this.below_screen = below_screen;
     }
 }
